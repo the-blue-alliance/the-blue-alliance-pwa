bad46085f1f5218169d8e2e6b05dae9a5a20088a
Eugene Fang <fang.eugene@gmail.com>
2019-05-31 08:47:37 -0700

    Add eslint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  webpack: config => {
+  webpack: (config, { dev }) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
+    }
+
+    if (dev) {
+      config.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      })
     }
 
     return config
