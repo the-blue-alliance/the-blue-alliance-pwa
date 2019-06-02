12b4677b9808011271a653c73c813c973678414d
Eugene Fang <fang.eugene@gmail.com>
2019-06-02 10:10:27 -0700

    Add canonical meta tag

--- components/Head.js
+++ components/Head.js
@@ -1,15 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { withRouter } from "next/router";
 import { default as NextHead } from "next/head";
 
-const Head = ({ children, title }) => (
+const Head = ({ router, children, title }) => (
   <NextHead>
     <title>{title && `${title} - `}The Blue Alliance</title>
+    <link
+      rel="canonical"
+      href={`https://www.thebluealliance.com${router.asPath}`}
+    />
+
     {children}
   </NextHead>
 );
 
 Head.propTypes = {
+  router: PropTypes.object,
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node)
@@ -17,4 +24,4 @@ Head.propTypes = {
   title: PropTypes.string
 };
 
-export default Head;
+export default withRouter(Head);
