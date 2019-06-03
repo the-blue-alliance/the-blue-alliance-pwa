49845834ac7e6c1a86abb7b1db53202183eb1c61
Eugene Fang <fang.eugene@gmail.com>
2019-06-02 21:21:16 -0700

    Share button on app bar

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "next/router";
 import Head from "next/head";
@@ -6,10 +6,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import IconButton from "@material-ui/core/IconButton";
+import Snackbar from "@material-ui/core/Snackbar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import RefreshIcon from "@material-ui/icons/Refresh";
+import ShareIcon from "@material-ui/icons/Share";
+import * as clipboard from "clipboard-polyfill";
 
 import TBALogoSVG from "../icons/tba_lamp.svg";
 
@@ -66,8 +69,24 @@ const Page = ({
   refreshFunction,
   isLoading
 }) => {
-  const canonicalUrl = `https://www.thebluealliance.com${router.asPath}`;
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarOpenedOnce, setSnackbarOpenedOnce] = useState(false);
   const classes = useStyles();
+
+  const handleShare = () => {
+    if (navigator.share) {
+      navigator.share({
+        title: document.title,
+        url: document.URL
+      });
+    } else {
+      clipboard.writeText(document.URL);
+      setSnackbarOpen(true);
+      setSnackbarOpenedOnce(true);
+    }
+  };
+
+  const canonicalUrl = `https://www.thebluealliance.com${router.asPath}`;
   return (
     <>
       <Head>
@@ -133,11 +152,23 @@ const Page = ({
               <RefreshIcon />
             </IconButton>
           )}
+          <IconButton color="inherit" onClick={handleShare} aria-label="Share">
+            <ShareIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <div className={classes.container}>
         <main className={classes.content}>{children}</main>
       </div>
+      {snackbarOpenedOnce && (
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          open={snackbarOpen}
+          onClose={() => setSnackbarOpen(false)}
+          autoHideDuration={2000}
+          message="Link copied!"
+        />
+      )}
     </>
   );
 };
