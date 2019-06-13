25976e188041647f365d7f4991fee58743c0b81a
Eugene Fang <fang.eugene@gmail.com>
2019-06-13 14:01:54 -0700

    Remove error testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Page from "../components/Page";
 import Link from "../components/Link";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const Home = () => {
-  const [error, setError] = useState(false);
-  if (error) {
-    throw new Error("RIP");
-  }
   return (
     <Page metaDescription="Team information and match videos and results from the FIRST Robotics Competition.">
       <Typography variant="h4">Home</Typography>
@@ -17,10 +12,6 @@ const Home = () => {
       <Link href="/event?eventKey=2019casj" as="/event/2019casj">
         2019casj
       </Link>
-      <br />
-      <Button variant="outlined" onClick={() => setError(true)}>
-        Error!
-      </Button>
     </Page>
   );
 };
