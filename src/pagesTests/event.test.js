450b6a656225cdd2bc7d8c99b96a08d8e95637f6
Eugene Fang <fang.eugene@gmail.com>
2019-06-24 17:18:07 -0700

    Add basic Event page test

---
+++ src/pagesTests/event.test.js
@@ -0,0 +1,31 @@
+/* eslint-env jest */
+import React from "react";
+import { Provider } from "react-redux";
+import { RouterContext } from "next-server/dist/lib/router-context";
+import { mount } from "enzyme";
+import getOrCreateStore from "../lib/store";
+import Event from "../pages/event.js";
+
+const reduxStore = getOrCreateStore();
+
+it("Renders without crashing", async () => {
+  const router = {
+    pathname: "/event",
+    route: "/event",
+    query: { eventKey: "2019casj" },
+    asPath: "/event",
+  };
+
+  const { ...initialProps } = await Event.getInitialProps({
+    reduxStore,
+    query: router.query,
+  });
+
+  mount(
+    <RouterContext.Provider value={router}>
+      <Provider store={reduxStore}>
+        <Event {...initialProps} />
+      </Provider>
+    </RouterContext.Provider>
+  );
+});
