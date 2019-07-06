f73e443b3b94cd1db1ce5b9456ac5f6342977417
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2019-07-05 20:15:32 -0700

    Cleanup

--- src/pages/teams.js
+++ src/pages/teams.js
@@ -1,51 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Page from "../components/Page";
-// import NoSsr from "@material-ui/core/NoSsr";
 import Typography from "@material-ui/core/Typography";
 import notFoundError from "../lib/notFoundError";
 import GroupedListCards from "../components/GroupedListCards";
-// import WindowScrollerList from "../components/WindowScrollerList";
-// import StickySectionHeader from "../components/StickySectionHeader";
 import TeamListItem from "../components/TeamListItem";
-
-// class ServerFallback extends React.Component {
-//   // Enables the hydration of client without re-rendering server-only fallback
-//   // See https://github.com/facebook/react/issues/6985#issuecomment-326526059
-//
-//   getExistingHtml(id) {
-//     if (typeof document === "undefined") return;
-//     const node = document.getElementById(id);
-//     return node && node.innerHTML;
-//   }
-//
-//   shouldComponentUpdate() {
-//     return false;
-//   }
-//
-//   render() {
-//     const { id, children } = this.props;
-//     const html = this.getExistingHtml(id);
-//
-//     if (html) {
-//       // Hydrate fallback on client without re-rendering
-//       return <div id={id} dangerouslySetInnerHTML={{ __html: html }} />;
-//     }
-//     if (process.browser) {
-//       // Don't render fallback on client
-//       return null;
-//     }
-//     // Render fallback on server
-//     return <div id={id}>{children}</div>;
-//   }
-// }
-// ServerFallback.propTypes = {
-//   id: PropTypes.string.isRequired,
-//   children: PropTypes.oneOfType([
-//     PropTypes.arrayOf(PropTypes.node),
-//     PropTypes.node,
-//   ]).isRequired,
-// };
 
 const Teams = ({ page, maxPage, teams }) => {
   const itemRenderer = React.useCallback(({ item: team, style }) => {
@@ -108,27 +67,6 @@ const Teams = ({ page, maxPage, teams }) => {
         ssrFallbackId="team-list-server-fallback"
         ssrGroup={page}
       />
-
-      {/*<NoSsr
-        fallback={
-          <ServerFallback id="team-list-server-fallback">
-            <Paper>
-              {teams.slice(minTeam - 1, maxTeam).map(team => (
-                <TeamListItem key={team.key} team={team} />
-              ))}
-            </Paper>
-          </ServerFallback>
-        }
-      >
-        <Paper>
-          <WindowScrollerList
-            rowCount={teams.length}
-            rowHeight={65}
-            rowRenderer={rowRenderer}
-            startingOffset={page === 0 ? null : (minTeam - 2) * 65}
-          />
-        </Paper>
-      </NoSsr>*/}
     </Page>
   );
 };
