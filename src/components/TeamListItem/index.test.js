423ff52ca0e3ab03557de8cab8330eb5ec8a222e
Eugene Fang <1421884+fangeugene@users.noreply.github.com>
2019-07-06 00:24:27 -0700

    Add basic ListItem tests

---
+++ src/components/TeamListItem/index.test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+import React from "react";
+import { shallow } from "enzyme";
+import Component from "./index.js";
+
+it("Renders without crashing", () => {
+  shallow(
+    <Component
+      team={{
+        // TODO: Use real data
+        key: "frc604",
+        name: "Sponsor 1, Sponsor 2",
+        nickname: "Quixilver",
+        location: "San Jose, CA, USA",
+      }}
+    />
+  );
+});
